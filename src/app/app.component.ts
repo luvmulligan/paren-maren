f378b0f2cb74af981e591f9251b7ce2a2b4f3b68
luvmulligan <lucia.velarde@lamercantil.com.ar>
2023-05-03 20:49:20 -0300

    feat: :art: refactor diceRoll, sounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,7 @@ export class Player {
 export class AppComponent implements OnInit {
   title = 'paren-maren';
   turnPlayer: Player;
-  canRoll: boolean = false;
-  canRoll2: boolean = false;
-  canRoll3: boolean = false;
-  canRoll4: boolean = false;
+  canRoll: boolean = true;
 
   gameBoard = {
     players: [],
@@ -29,110 +26,121 @@ export class AppComponent implements OnInit {
   rollResult3 = 0;
   rollResult4 = 0;
   turnDices = [];
+  turnValue;
+  currentPlayer;
+  x;
 
   ngOnInit() {}
 
+  // myFunction().then(
+  //   function(value) {myDisplayer(value);}
+  // );
+
+  // this.turnDices.forEach((val) => {
+  //   total += val.value;
+  //   console.log(total);
+  // }
+
   switchTurn() {
+    this.canRoll = true;
+    this.currentPlayer = this.gameBoard.players[this.gameBoard.currentTurn];
+    this.currentPlayer.score = this.score;
+    localStorage.setItem('Game', JSON.stringify(this.gameBoard));
+    this.turnDices = [];
     this.gameBoard.currentTurn = (this.gameBoard.currentTurn + 1) % this.gameBoard.players.length;
-    this.rollDice1();
   }
 
   // Define the function for updating the score
-  updateScore(playerIndex, diceRoll) {
-    this.gameBoard.players[playerIndex].score += diceRoll;
+  updateScore(playerIndex, score: any) {
+    this.gameBoard.players[playerIndex].score = score;
+    console.log(this.gameBoard.players[playerIndex].score);
   }
+
+  playSound() {
+    this.x = document.getElementById('dice-audio');
+    this.x.load();
+    this.x.play();
+  }
+
+  rollDice1() {
+    this.playSound();
+    this.rollResult = 0;
+    if (this.turnDices.length === 4) {
+      return;
+    } else {
+      this.currentPlayer = this.gameBoard.players[this.gameBoard.currentTurn];
+
+      // let currentPlayer = JSON.parse(localStorage.getItem('Players'))[0 + 1];
+      let rollDice = Math.floor(Math.random() * (6 - 1) + 1);
+      if (rollDice >= 4) {
+        this.canRoll = true;
+      }
+      if (rollDice < 4) {
+        this.canRoll = false;
+      }
+      this.rollResult = rollDice;
+      this.score = this.score + this.rollResult;
+      // this.updateScore(this.gameBoard.currentTurn, this.rollResult);
+
+      this.turnDices.push({ id: 1, rollResult: this.rollResult });
+      // this.score = this.score + this.rollResult;
+    }
+  }
+
+  // rollDice2() {
+  //   let rollDice = Math.floor(Math.random() * (6 - 1) + 1);
+  //   if (rollDice >= 4) {
+  //     this.canRoll2 = true;
+  //   }
+  //   if (rollDice < 4) {
+  //     this.canRoll2 = false;
+  //   }
+  //   this.rollResult2 = rollDice;
+
+  //   this.score = this.score + rollDice;
+  //   console.log(this.score);
+  //   this.turnDices.push({ id: 2, rollResult: this.rollResult2 });
+  //   // this.updateScore(this.gameBoard.currentTurn, rollDice);
+  //   // this.turnDices.push({ value: rollDice });
+  // }
+  // rollDice3() {
+  //   let rollDice = Math.floor(Math.random() * (6 - 1) + 1);
+  //   if (rollDice >= 4) {
+  //     this.canRoll3 = true;
+  //   }
+  //   if (rollDice < 4) {
+  //     this.canRoll3 = false;
+  //   }
+  //   this.rollResult3 = rollDice;
+
+  //   this.score = this.score + rollDice;
+  //   console.log(this.score);
+  //   this.turnDices.push({ id: 3, rollResult: this.rollResult3 });
+  //   // this.score = this.score + this.rollResult3;
+  // }
+  // rollDice4() {
+  //   let rollDice = Math.floor(Math.random() * (6 - 1) + 1);
+  //   if (rollDice >= 4) {
+  //     this.canRoll4 = true;
+  //   }
+  //   if (rollDice < 4) {
+  //     this.canRoll4 = false;
+  //   }
+  //   this.rollResult4 = rollDice;
+
+  //   this.score = this.score + rollDice;
+  //   console.log(this.score);
+  //   this.turnDices.push({ id: 4, rollResult: this.rollResult4 });
+  //   // this.updateScore(this.gameBoard.currentTurn, rollDice);
+
+  //   // this.score = this.score + this.rollResult4;
+  // }
 
   newPlayer() {
     let newPlayer: Player = new Player();
     newPlayer.name = prompt('');
     this.gameBoard.players.push(newPlayer);
     newPlayer.id = this.gameBoard.players.length;
-    console.log(this.gameBoard.players);
-    localStorage.setItem('Players', JSON.stringify(this.gameBoard.players));
-  }
-  endTurn() {
-    let currentPlayer = JSON.parse(localStorage.getItem('Players'))[0];
-  }
-
-  // Define the function for playing a turn
-  playTurn() {
-    // Roll the dice
-    let diceRoll = this.rollDice1();
-
-    // Update the score
-    var currentPlayer = this.gameBoard.players[this.gameBoard.currentTurn];
-    this.updateScore(this.gameBoard.currentTurn, diceRoll);
-
-    // Switch turns
-    this.switchTurn();
-
-    // Display the game board
-
-    // Return the dice roll and the updated game board
-    return {
-      diceRoll: diceRoll,
-      gameBoard: this.gameBoard
-    };
-  }
-
-  rollDice1() {
-    this.turnDices = [];
-    this.rollResult = 0;
-    this.rollResult2 = 0;
-    this.rollResult3 = 0;
-    this.rollResult4 = 0;
-    var currentPlayer = this.gameBoard.players[this.gameBoard.currentTurn];
-
-    // let currentPlayer = JSON.parse(localStorage.getItem('Players'))[0 + 1];
-    this.rollResult = Math.floor(Math.random() * (6 - 1) + 1);
-    if (this.rollResult >= 4) {
-      this.canRoll = true;
-    }
-    if (this.rollResult < 4) {
-      this.canRoll = false;
-    }
-    this.turnDices.push({ value: this.rollResult });
-    this.score = this.score + this.rollResult;
-    console.log(currentPlayer);
-  }
-
-  rollDice2() {
-    this.rollResult2 = Math.floor(Math.random() * (6 - 1) + 1);
-    if (this.rollResult2 >= 4) {
-      this.canRoll2 = true;
-    }
-    if (this.rollResult2 < 4) {
-      this.canRoll2 = false;
-    }
-    this.turnDices.push({ value: this.rollResult2 });
-
-    this.score = this.score + this.rollResult2;
-    console.log(this.turnDices);
-  }
-  rollDice3() {
-    this.rollResult3 = Math.floor(Math.random() * (6 - 1) + 1);
-    if (this.rollResult3 >= 4) {
-      this.canRoll3 = true;
-    }
-    if (this.rollResult3 < 4) {
-      this.canRoll3 = false;
-    }
-    this.turnDices.push({ value: this.rollResult3 });
-
-    this.score = this.score + this.rollResult3;
-    console.log(this.turnDices);
-  }
-  rollDice4() {
-    this.rollResult4 = Math.floor(Math.random() * (6 - 1) + 1);
-    if (this.rollResult4 >= 4) {
-      this.canRoll4 = true;
-    }
-    if (this.rollResult4 < 4) {
-      this.canRoll4 = false;
-    }
-    this.turnDices.push({ value: this.rollResult4 });
-
-    this.score = this.score + this.rollResult4;
-    console.log(this.turnDices);
+    localStorage.setItem('Game', JSON.stringify(this.gameBoard));
   }
 }
